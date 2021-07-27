964f96225750930c0730319114b0bc5dbb9c6b7a
timlock <81235388+timlock@users.noreply.github.com>
2021-07-27 13:30:39 +0200

    ProjectStart

---
+++ MainApp.cpp
@@ -0,0 +1,10 @@
+//
+// Created by tim-l on 27.07.2021.
+//
+
+
+
+int main(){
+    
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/Small_Neural_Network.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Small_Neural_Network.dir/MainApp.cpp.obj"
+  "Small_Neural_Network.exe"
+  "Small_Neural_Network.exe.manifest"
+  "Small_Neural_Network.pdb"
+  "libSmall_Neural_Network.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Small_Neural_Network.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
